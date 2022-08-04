83f187ba9b35cdb9f181033d91f9831a63925d62
dlstenbro <dlstenbro@email.com>
2022-08-04 18:42:25 -0500

    Add initial TwitchAPIModel and Controller
    * Add common namespaces that will be used for controllers, models, and
      common utils.
    * Add properties for json data from TwitchAPI streams endpoint
    * Add OAuthConnector class to handle/retrieve OAuth 2.0 tokens

---
+++ Controllers/TwitchAPIController.cs
@@ -0,0 +1,59 @@
+﻿using ConcurrenceAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using ConcurrenceAPI.Common;
+
+namespace ConcurrenceApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TwitchAPIController : ControllerBase
+    {
+        #region Enviroment Variables
+
+        private string api_client_id = Environment.GetEnvironmentVariable("TWITCH_CLIENT_ID");
+        private string api_access_token = Environment.GetEnvironmentVariable("TWITCH_CLIENT_SECRET");
+        
+        #endregion Enviroment Variables
+
+        #region API Endpoints
+        
+        private string api_streams_url = @"https://api.twitch.tv/helix/streams";
+        
+        #endregion API Endpoints
+
+        private OAuthConnector connector;
+        private TwitchAPIModel model;
+
+        public TwitchAPIController(TwitchAPIModel model)
+        {
+            this.model = model;
+
+            connector = new OAuthConnector(api_streams_url);
+            connector.GetAuthToken("client_credientials", api_client_id, api_access_token);
+        }
+
+        private readonly ILogger<TwitchAPIController> _logger;
+
+        public TwitchAPIController(ILogger<TwitchAPIController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<TwitchAPIModel> Get()
+        {
+            // request all streams and update the model with the new list
+            return Enumerable.Range(1, 5).Select(index => new TwitchAPIModel
+            {
+                //Date = DateTime.Now.AddDays(index),
+                //TemperatureC = rng.Next(-20, 55),
+                //Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ Models/TwitchAPIModel.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using ConcurrenceAPI.Common;
+
+namespace ConcurrenceAPI.Models
+{
+    public class TwitchAPIModel
+    {
+        public List<TwitchStreamMeta> streams;
+
+        #region Constructor
+        public TwitchAPIModel()
+        {
+
+        }
+        #endregion Constructor
+
+        #region Methods
+
+        #endregion Methods
+
+    }
+
+    public class TwitchStreamMeta
+    {
+        #region Constructor
+        public TwitchStreamMeta()
+        {
+
+        }
+        #endregion Constructor
+
+        #region Properties
+        public string id { get; set; }
+        public int user_id { get; set; }
+        public int user_login { get; set; }
+        public string user_name { get; set; }
+        public int game_id { get; set; }
+        public string game_name { get; set; }
+        public string type { get; set; }
+        public string title { get; set; }
+        public int viewer_count { get; set; }
+        public DateTime started_at { get; set; }
+        public string language { get; set; }
+        public string thumbnail_url { get; set; }
+        public List<string> tag_ids { get; set; }
+        public bool is_mature { get; set; }
+        #endregion Properties
+    }
+}
