3bc75865c6fb3e161624e6156e0252294b11db4b
dlstenbro <danielstenbro101@hotmail.com>
2022-08-11 21:34:07 -0500

    add properties for AuthToken

---
+++ ConcurrenceAPI/Common/AuthToken.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConcurrenceAPI.Common
+{
+    public class AuthToken
+    {
+        public string access_token { get; set; }
+        public int expires_in { get; set; }
+        public string token_type { get; set; }
+
+    }
+}
+
