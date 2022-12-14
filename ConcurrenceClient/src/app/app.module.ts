b6578425dcc199bb8f5d941d5890843b4fee421b
dlstenbro <danielstenbro101@hotmail.com>
2022-12-13 23:35:41 -0600

    add httpclient module to make requests to api

--- ConcurrenceClient/src/app/app.module.ts
+++ ConcurrenceClient/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
 import { StreamsComponent } from './streams/streams.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { AppRoutingModule } from './app-routing.module';
+
 
 @NgModule({
   declarations: [
@@ -14,6 +16,7 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
--- ConcurrenceClient/src/app/streams/streams.component.ts
+++ ConcurrenceClient/src/app/streams/streams.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-streams',
@@ -8,10 +9,64 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class StreamsComponent implements OnInit {
 
-  constructor() { }
+  public api_url: string = "http://localhost:80";
+  public platform = "Twitch";
+  public title = "Streams";
+  public streams : Streams | undefined;
+
+  constructor(private http : HttpClient) {
+
+    // get streams from API
+    var headers = {
+      "Allow-Access-Control-Origin" : "*",
+      "Content-Type"                : "application/json",
+      "method"                      : "GET"
+    };
+
+    var request = http.get(this.api_url, { headers }).subscribe((data: any) => {
+      console.log(data);
+    });
+
+
+
+    //this.streams = this.GetReponse(this.api_url).then(res => res.parse("twitchStreams"));
+    //var response = this.GetResponse(this.api_url);
+    //console.log(response);
+  }
 
   ngOnInit(): void {
+
   }
-  platform = "Twitch"
-  title = "Streams"
+
+/*
+  GetResponse(url: string): Promise<any> {
+    var headers = { "Content-Type": "application/json", "method": "GET" };
+    return fetch(url, { mode : "no-cors", headers : headers })
+      .then(res => res.json())
+      .catch(error => {
+        console.log(error)
+      });
+  }
+  */
+}
+
+interface Streams {
+  "twitchStreams" : any;
+}
+
+interface Stream {
+  id            : string,
+  user_id	    : string,
+  user_login    : string,
+  user_name     : string,
+  game_id       : number,
+  game_name	    : string,
+  type          : string,
+  title	        : string,
+  viewer_count  : number,	
+  started_at    : string,
+  language      : string,
+  thumbnail_url : string,
+  tag_ids       : string,
+  is_mature	    : boolean
 }
