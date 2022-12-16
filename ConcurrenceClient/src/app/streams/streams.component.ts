feb48d1dab1188f288d5f3b42a52b26f7094e4ec
dlstenbro <danielstenbro101@hotmail.com>
2022-12-16 15:22:01 -0600

    Add json response to webpage with a basic html tables

--- ConcurrenceClient/src/app/streams/streams.component.ts
+++ ConcurrenceClient/src/app/streams/streams.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
@@ -9,64 +8,46 @@ import { HttpClient } from '@angular/common/http';
 })
 export class StreamsComponent implements OnInit {
 
-  public api_url: string = "http://localhost:80";
+  public api_url: string = "https://localhost:5001";
   public platform = "Twitch";
   public title = "Streams";
-  public streams : Streams | undefined;
+  public streams ?: PlatformStreamList;
+  public test : any
 
-  constructor(private http : HttpClient) {
+  public constructor( private http : HttpClient ) { }
 
-    // get streams from API
+  public ngOnInit(): void {
+    this.getResponse(this.api_url);
+  }
+
+  public getResponse(api_url : string) {
     var headers = {
-      "Allow-Access-Control-Origin" : "*",
-      "Content-Type"                : "application/json",
-      "method"                      : "GET"
+      "Content-Type": "application/json",
+      "method": "GET"
     };
 
-    var request = http.get(this.api_url, { headers }).subscribe((data: any) => {
-      console.log(data);
-    });
-
-
-
-    //this.streams = this.GetReponse(this.api_url).then(res => res.parse("twitchStreams"));
-    //var response = this.GetResponse(this.api_url);
-    //console.log(response);
+    this.http.get(api_url, { headers })
+      .subscribe((data: any) => this.streams = data);
   }
-
-  ngOnInit(): void {
-
-  }
-
-/*
-  GetResponse(url: string): Promise<any> {
-    var headers = { "Content-Type": "application/json", "method": "GET" };
-    return fetch(url, { mode : "no-cors", headers : headers })
-      .then(res => res.json())
-      .catch(error => {
-        console.log(error)
-      });
-  }
-  */
 }
 
-interface Streams {
-  "twitchStreams" : any;
+interface PlatformStreamList {
+  "twitchStreams" ?: Array<Stream>;
 }
 
 interface Stream {
-  id            : string,
-  user_id	    : string,
-  user_login    : string,
-  user_name     : string,
-  game_id       : number,
-  game_name	    : string,
-  type          : string,
-  title	        : string,
-  viewer_count  : number,	
-  started_at    : string,
-  language      : string,
-  thumbnail_url : string,
-  tag_ids       : string,
-  is_mature	    : boolean
+  id            ?: string,
+  user_id	    ?: string,
+  user_login    ?: string,
+  user_name     ?: string,
+  game_id       ?: number,
+  game_name	    ?: string,
+  type          ?: string,
+  title	        ?: string,
+  viewer_count  ?: number,	
+  started_at    ?: string,
+  language      ?: string,
+  thumbnail_url ?: string,
+  tag_ids       ?: string,
+  is_mature	    ?: boolean
 }
